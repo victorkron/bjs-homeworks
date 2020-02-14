8b70dff3f7cdc8c4ebe6c3b4f9cb1722515201a7
Karim <k.kron2015@yandex.ru>
2020-02-14 09:46:00 +0300

    1.(1,2,3) выпонены

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -1,17 +1,32 @@
 function averageMark(a, g, p) {
     // реализуйте здесь задание №1
+    let algebra = a;
+    let geography = g;
+    let physics = p;
+    let res = (a + g + p) / 3;
     // раскомментируйте следующую строку и верните результат (строку выше и эту удалите)
-    // return 0;
+    return res;
 }
 
 function sayHello(userName) {
     // реализуйте здесь задание №2
+    let myName = userName;
+    let message = `Привет, мир! Меня зовут ${myName}`;
     // раскомментируйте следующую строку и верните результат (строку выше и эту удалите)
-    // return "Привет";
+
+    return message;
 }
 
 function calculateFormula() {
     // реализуйте здесь задание №3
+    let x;
+    let y;
+    let z;
+    x = 2;
+    y = 22;
+    z = 0;
+    let result = x * y + 5 * z + x - 1;
     // раскомментируйте следующую строку и верните результат (строку выше и эту удалите)
-    // return 0;
+
+    return result;
 }
