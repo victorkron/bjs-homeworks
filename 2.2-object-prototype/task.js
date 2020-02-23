a5de789b01cd3fb1731972a7ce43ed1766c1b4a5
Karim <k.kron2015@yandex.ru>
2020-02-23 10:46:46 +0300

    2.2 done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,38 @@
 function getAnimalSound(animal) {
+    'use strict';
     // код для задачи №1 писать здесь
-    // return animalSound;
+    let animalSound;
+    if (animal == undefined) { animalSound = null; }
+    else { animalSound = animal.sound; }
+    return animalSound;
 }
 
 function getAverageMark(marks) {
+    'use strict';
     // код для задачи №2 писать здесь
-    // return averageMark
+    let averageMark = 0;
+    if (marks.length != 0) {
+      for (let variable of marks) {
+        averageMark += Number(variable);
+      }
+      averageMark = Math.round(averageMark / marks.length);
+    }
+    else {
+
+    }
+    return averageMark;
 }
 
 function checkBirthday(birthday) {
+    'use strict';
     // код для задачи №3 писать здесь
-    // return verdict
+    let now = new Date();
+    birthday = new Date(birthday);
+
+    let diff = now - birthday;
+
+    let age = (diff / ((1000 * 60 * 60 * 24 * (365 * 4 + 1)) / 4));
+
+    let verdict = Boolean(Math.floor(age / 18));
+    return verdict;
 }
